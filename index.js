20fdd9c725cb5b7127d5b7504f5707f56f9183e0
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-07 12:21:25 -0800

    Update index.js

--- index.js
+++ index.js
@@ -375,20 +375,37 @@ client.on('message', message => {
 });
 
 
-var prefix = "/";
 client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
+    var prefix = "."
+    let command = message.content.split(" ")[0];
   command = command.slice(prefix.length);
 
   let args = message.content.split(" ").slice(1);
 
-  if (command == "say") {
-   message.channel.sendMessage(args.join("  "))
-  }
-});
+
+if(command == "draw") {
+    var Canvas = require('canvas')
+  , Image = new Canvas.Image
+  , canvas = new Canvas(450, 170)
+  , ctx = canvas.getContext('2d');
+  ctx.font = '30px Impact';
+  let args = message.content.split(" ").slice(1);
+  
+Image.src = canvas.toBuffer();
+
+    console.log(Image);
+ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
+ctx.fillText(args.join("  "),110, 70);
+
+
+ctx.beginPath();
+ctx.lineTo(50, 102);
+ctx.stroke();
+
+message.channel.sendFile(canvas.toBuffer());
+}
+}).on('ready', () => {
+    console.log(`Im ready ${client.user.username}`)
 });
 
 
