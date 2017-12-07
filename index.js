833fe2dabfc9aa82d255d3ef6a844a4cfabf5994
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-07 12:06:09 -0800

    Update index.js

--- index.js
+++ index.js
@@ -339,6 +339,52 @@ client.on('guildCreate', guild => {
 });
 
 
+bot.on('message', message => {
+    if (message.content === prefix + 'avatar') {
+        const Embeds = new Discord.RichEmbed()
+        .setColor(0x00AE86)
+        .setImage(`${message.author.avatar}`)
+        message.channel.sendMessage({Embeds});
+    }
+})
+
+
+client.on('message', message => {
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("**.bc <message>**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var bc = new Discord.RichEmbed()
+                .setAuthor(message.author.username, message.author.avatarURL)
+                .addField('** الـسيرفر**', `${message.guild.name}`,true)
+                .addField(' **الـمرسل **', `${message.author.username}#${message.author.discriminator}`,true)
+                .addField(' **الرسالة** ', args)
+                .setThumbnail(message.guild.iconURL)
+                .setColor('RANDOM')
+                m.send(`${m}`,{embed: bc});
+            });
+            const AziRo = new Discord.RichEmbed()
+            .setAuthor(message.author.username, message.author.avatarURL)   
+            .setTitle('✔️ | جاري ارسال رسالتك ') 
+            .addBlankField(true)
+            .addField('👥 | عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)        
+            .addField('📋| الرسالة ', args)
+            .setColor('RANDOM')  
+            message.channel.sendEmbed(AziRo);          
+        }
+        } else {
+            return;
+        }
+    });
+
+
 client.login(process.env.BOT_TOKEN);
 
 var prefix = '/'
