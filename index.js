feb0e99fd9289bd456b1e32e834912278f41c0c2
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-07 12:38:49 -0800

    Update index.js

--- index.js
+++ index.js
@@ -375,6 +375,24 @@ client.on('message', message => {
 });
 
 
+client.on('message', message => {
+              if (!message.channel.guild) return;
+      if(message.content =='member')
+      var IzRo = new Discord.RichEmbed()
+      .setThumbnail(message.author.avatarURL)
+      .setFooter(message.author.username, message.author.avatarURL) 
+      .setTitle('🌷| Members info')
+      .addBlankField(true)
+      .addField('📗| Online',
+      `${message.guild.members.filter(m=>m.presence.status == 'online').size}`)
+      .addField('📕| DND',`${message.guild.members.filter(m=>m.presence.status == 'dnd').size}`)
+      .addField('📙| Idle',`${message.guild.members.filter(m=>m.presence.status == 'idle').size}`)
+      .addField('📓| Offline',`${message.guild.members.filter(m=>m.presence.status == 'offline').size}`)
+      .addField('➡| Server Members',`${message.guild.memberCount}`)
+      message.channel.send(IzRo);
+    });
+
+
 client.login(process.env.BOT_TOKEN);
 
 var prefix = '/'
