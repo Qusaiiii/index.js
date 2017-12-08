7e21c649da2abc6c0a71488c7f8ac02ec09450b1
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-08 08:22:01 -0800

    Update index.js

--- index.js
+++ index.js
@@ -495,7 +495,7 @@ message.channel.sendEmbed(embed);
 
 client.on('message', message => {
 let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('/bc-users')){
+if (message.content.startsWith('*bc-users')){
  if(!message.author.id === '324672376455299074,334585041801969687') return;
 message.channel.sendMessage('تم , جار أرسال الرسالة')
 client.users.forEach(m =>{
