0bf5085a0296e14ae25768e6b340ef44d746e2e2
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-10 07:51:30 -0800

    Update index.js

--- index.js
+++ index.js
@@ -522,6 +522,30 @@ if(command === `${prefix}message`) {
  }
 });
 
+bot.on('message', message => {
+    if (message.content.startsWith("*stats")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .addField('Uptime', timeCon(process.uptime()), true)
+            .addField('RAM Usage', `${(process.memoryUsage().rss / 1048576).toFixed()}MB`, true)
+            .addField('Guild Count', bot.guilds.size, true)
+    })
+}
+});
+
+
+function timeCon(time) {
+    let days = Math.floor(time % 31536000 / 86400)
+    let hours = Math.floor(time % 31536000 % 86400 / 3600)
+    let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60)
+    let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60)
+    days = days > 9 ? days : '0' + days
+    hours = hours > 9 ? hours : '0' + hours
+    minutes = minutes > 9 ? minutes : '0' + minutes
+    seconds = seconds > 9 ? seconds : '0' + seconds
+    return `${days > 0 ? `${days}:` : ''}${(hours || days) > 0 ? `${hours}:` : ''}${minutes}:${seconds}`
+}
+
 
 client.login(process.env.BOT_TOKEN);
 
