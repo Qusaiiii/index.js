2ee34e86a66c046e15e101a8f2b2f5904fbf60b5
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-10 12:21:28 -0800

    Update index.js

--- index.js
+++ index.js
@@ -459,23 +459,7 @@ client.on("message", message => {
    message.channel.send({embed});
       }
   });
-
-
-client.on('message', message => {
-     if (message.content === "*bot") {
-            if(!message.channel.guild) return message.reply('** This command only for servers **');
-     let embed = new Discord.RichEmbed()
-  .setColor('RANDOM')
-  .addField("**عدد السيرفرات الي فيها البوت:**" , client.guilds.size)
-  .addField("**المستخدمين:**", client.users.size)
-  .addField("**قنوات:**", client.channels.size)
-  .setTimestamp()
-message.channel.sendEmbed(embed);
-    }
-});
-
-})
-            
+  
 	
 client.login('process.env.BOT_TOKEN');
 
