ed4b6ed23f8fab86da8d1bffa2d264249d669ad3
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-07-23 22:25:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -60,12 +60,38 @@ client.on('message',message => {
 });
 client.on('ebnklb',function(ebnklb) {
     
-    if(ebnklb.content.startsWith("@470918643224674304")) {
-        ebnklb.channel.send('Hey Im **RqlixMC-Systeù**  A Nice Bot Developed By:`ImRoyal_Radddar`')
+    if(ebnklb.content.startsWith("<@470918643224674304>")) {
+        ebnklb.channel.send('Hey Im **RqlixMC-System**  A Nice Bot Developed By:`ImRoyal_Radddar`')
         ebnklb.channel.send('My Prefix `#`')
 
     }
 });
+client.on("message", message => {
+    var prefix = "&";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('  ⚠  ** **');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل بنجاح",
+        footer: {
+          text: "RqlixMC v0.1"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
 
+     
+
+        
+    
+
+         
+     });
 
 client.login('NDcwOTE4NjQzMjI0Njc0MzA0.DjfBsA.L5yvoZ4mp4vtNlw_1qr_FQ4JfBQ');
