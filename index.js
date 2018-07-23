17de9fa291e44b31a1e2572e681fa03a3f0bb63f
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-07-23 21:54:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,37 +3,8 @@
 	const fs = require('fs');
 	const prefix = "#"
 	const moment = require('moment');
-const sql = require("sqlite");
-client.on("message",async message => {
-    if (message.content.startsWith(prefix + "at")) {
-         var ids = [
-            "20",
-            "1",
-            "13",
-            "18",
-            "17",
-            "9",
-            "31",
-            "22",
-            "23",
-            "2",
-            "11",
-            "19",
-            "24",
-            "25",
-            "12",
-            "33"
-            ]
-            const randomizer = Math.floor(Math.random()*ids.length);
-            const args = message.content.split(" ").slice(1).join(" ")
-    if (!args) return message.channel.send("**اكتب محتوي الانجاز**");
-    const image = new Discord.Attachment(`https://www.minecraftskinstealer.com/achievement/a.php?i=${ids[randomizer]}&h=Achievement Get!&t=${args}`, "achievement.png");
-message.channel.send(image)
-    }
-});
+        const sql = require("sqlite");
 
- 
-});
   client.on('message', async message => {
   if(message.content.startsWith("#modapply")) {
     message.channel.send("**:writing_hand: ارسل اسمك في ماين كرافت **").then(e => {
@@ -127,7 +98,36 @@ client.on('message',async message => {
     }
 
 });
-  
+  client.on("message",async message => {
+    if (message.content.startsWith(prefix + "at")) {
+         var ids = [
+            "20",
+            "1",
+            "13",
+            "18",
+            "17",
+            "9",
+            "31",
+            "22",
+            "23",
+            "2",
+            "11",
+            "19",
+            "24",
+            "25",
+            "12",
+            "33"
+            ]
+            const randomizer = Math.floor(Math.random()*ids.length);
+            const args = message.content.split(" ").slice(1).join(" ")
+    if (!args) return message.channel.send("**اكتب محتوي الانجاز**");
+    const image = new Discord.Attachment(`https://www.minecraftskinstealer.com/achievement/a.php?i=${ids[randomizer]}&h=Achievement Get!&t=${args}`, "achievement.png");
+message.channel.send(image)
+    }
+});
+
+ 
+});
 
 client.login('NDcwOTE4NjQzMjI0Njc0MzA0.DjfBsA.L5yvoZ4mp4vtNlw_1qr_FQ4JfBQ');
 
