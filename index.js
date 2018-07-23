03c0a7b781a890b6c6c322458e24a55cfe42eed1
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-07-23 21:38:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,79 @@
 	const fs = require('fs');
 	const prefix = "#"
 	const moment = require('moment');
+var prefix = "#"
+const sql = require("sqlite");
+client.on("message",async message => {
+    if (message.content.startsWith(prefix + "at")) {
+         var ids = [
+            "20",
+            "1",
+            "13",
+            "18",
+            "17",
+            "9",
+            "31",
+            "22",
+            "23",
+            "2",
+            "11",
+            "19",
+            "24",
+            "25",
+            "12",
+            "33"
+            ]
+            const randomizer = Math.floor(Math.random()*ids.length);
+            const args = message.content.split(" ").slice(1).join(" ")
+    if (!args) return message.channel.send("**اكتب محتوي الانجاز**");
+    const image = new Discord.Attachment(`https://www.minecraftskinstealer.com/achievement/a.php?i=${ids[randomizer]}&h=Achievement Get!&t=${args}`, "achievement.png");
+message.channel.send(image)
+    }
+});
+
+ 
+});
+  client.on('message', async message => {
+  if(message.content.startsWith("#modapply")) {
+    message.channel.send("**:writing_hand: ارسل اسمك في ماين كرافت **").then(e => {
+    let filter = m => m.author.id === message.author.id
+    let lan = '';
+    let md = '';
+    let br = '';
+    let chaLan = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+    .then(collected => {
+      lan = collected.first().content
+      collected.first().delete()
+e.edit(`**:ارسل لماذا تريد ان تصبح مراقب**`)
+let chaMd = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+.then(co => {
+  md = co.first().content
+        co.first().delete()
+        e.edit(`**هل لديك خبرة سابقة و في اية سيرفر**`)
+let br = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })//By Codes , - ST I EdiTeD , .#4968//By Codes , - ST I EdiTeD , .#4968
+.then(col => {
+  br = col.first().content
+        col.first().delete()
+e.edit("**جاري التقديم علي طلبك...**").then(b => {
+        setTimeout(() => {
+  b.edit(`**تم التقديم وسيتم الرد فـ اقرب وقت**`)
+        },2000);
+var gg = message.guild.channels.find('name', 'التقديمات')
+if(!gg) return;//By Codes , - ST I EdiTeD , .#4968//By Codes , - ST I EdiTeD , .#4968
+if(gg) {
+gg.send({embed : new Discord.RichEmbed()//By Codes , - ST I EdiTeD , .#4968//By Codes , - ST I EdiTeD , .#4968
+.setDescription(`**اسم ماين كرافت : \n ${lan}\n سبب التقديم :\n ${md} \nالخبرة  :\n ${br}  **`)  
+          .setFooter(`RqlixMC v0.1`)//By Codes , - ST I EdiTeD , .#4968//By Codes , - ST I EdiTeD , .#4968//By Codes , - ST I EdiTeD , .#4968
+.setTimestamp()//By Codes , - ST I EdiTeD , .#4968//By Codes , - ST I EdiTeD , .#4968//By Codes , - ST I EdiTeD , .#4968//By Codes , - ST I EdiTeD , .#4968
+});
+}        
+})
+})
+})//By Codes , - ST I EdiTeD , .#4968
+})
+})
+ }//By Codes ,
+
 	var request = require('request');
 
 var mcCommand = '/minecraft'; // Command for triggering
@@ -162,37 +235,5 @@ mentionned.send(` :credit_card: | Transfer Receipt \`\`\`You have received ${arg
       });
 
 
- var prefix = "#"
-const sql = require("sqlite");
-client.on("message",async message => {
-    if (message.content.startsWith(prefix + "at")) {
-         var ids = [
-            "20",
-            "1",
-            "13",
-            "18",
-            "17",
-            "9",
-            "31",
-            "22",
-            "23",
-            "2",
-            "11",
-            "19",
-            "24",
-            "25",
-            "12",
-            "33"
-            ]
-            const randomizer = Math.floor(Math.random()*ids.length);
-            const args = message.content.split(" ").slice(1).join(" ")
-    if (!args) return message.channel.send("**اكتب محتوي الانجاز**");
-    const image = new Discord.Attachment(`https://www.minecraftskinstealer.com/achievement/a.php?i=${ids[randomizer]}&h=Achievement Get!&t=${args}`, "achievement.png");
-message.channel.send(image)
-    }
-});
-
- 
-});
 
 client.login(process.env.BOT_TOKEN);
