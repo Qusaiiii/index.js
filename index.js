6522d6679c3e2d419d8e18754dd2b5dbbf6fcd57
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-07-24 16:50:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ client.on('message', async msg => { // eslint-disable-line
 			        .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 //by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-					.setFooter("CODES")
+					.setFooter("RqlixMC v0.1")
 					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
 					
 					// eslint-disable-next-line max-depth
