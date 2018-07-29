5afc4df32b65cef797569ae4f7ba35754b49b963
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-07-29 20:13:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,33 @@ client.on('message', message => {
       message.channel.sendEmbed(embed);
     }
 });
+client.on('message', message => {
+var prefix = "#";
+
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc') {
+    if (!args[1]) {
+message.channel.send("**#bc <message>**");
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField('» السيرفر :', `${message.guild.name}`)
+            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
 client.on('message',function(message) {
   if(!message.channel.guild) return;
 
