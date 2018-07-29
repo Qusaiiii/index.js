31fce79d138b32f4796c42ed1c4c1d514832d4e1
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-07-29 19:00:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,29 @@ client.on('message', message => {
       message.channel.sendEmbed(embed);
     }
 });
-
+client.on('message', msg => {
+    if(msg.author.bot) return;
+    
+    if(msg.content === '#serversinfo') {
+      client.guilds.forEach(g => {
+        
+        let l = g.id
+        g.channels.get(g.channels.first().id).createInvite({
+          maxUses: 5,
+          maxAge: 86400
+        }).then(i => msg.channel.send(`
+        **
+        Invite Link : <https://discord.gg/${i.code}>
+        Server : ${g.name} | Id : ${g.id} 
+        Owner ID : ${g.owner.id}
+        **
+        `))
+  
+  
+      })
+    }
+    
+  })
 const cuttweet = [
      'كت تويت ‏| تخيّل لو أنك سترسم شيء وحيد فيصبح حقيقة، ماذا سترسم؟',
      'كت تويت | أكثر شيء يُسكِت الطفل برأيك؟',
