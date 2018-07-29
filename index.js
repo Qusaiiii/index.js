69881e5cc296852bdefee50e085aa4ee783b51bd
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-07-29 16:04:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,22 @@ client.channels.find('id', '473104410571177986').setName("「 This Server Us");
 client.channels.find('id', '473104410571177986').setName("「 This Server Useing Frix Premium 」");
   }, 3000);
 });
+client.on('message', ra3d => {
+var prefix = "#";
+                        let args = ra3d.content.split(" ").slice(1).join(" ")
+if(ra3d.content.startsWith(prefix + 'cc')) {
+    if(!args) return ra3d.channel.send('`يرجي اختيار كم لون `');
+             if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`**⚠ | `[MANAGE_ROLES]` لا يوجد لديك صلاحية**'); 
+              ra3d.channel.send(`**✅ |Created __${args}__ Colors**`);
+                  setInterval(function(){})
+                    let count = 0;
+                    let ecount = 0;
+          for(let x = 1; x < `${parseInt(args)+1}`; x++){
+            ra3d.guild.createRole({name:x,
+              color: 'RANDOM'})
+              }
+            }
+       });
 client.on('message', async message => {
     let date = moment().format('Do MMMM YYYY , hh:mm');
     let User = message.mentions.users.first();
