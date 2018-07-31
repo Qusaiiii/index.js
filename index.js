f6117bd1f27205571cef1122fe328c2dc1d17f78
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-07-31 22:06:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,266 @@ client.on('message', message => {
       message.channel.sendEmbed(embed);
     }
 });
+let giveaways = new Map();
+
+exports.exec = async (Bastion, message, args) => {
+  try {
+    if (args.item) {
+      // Allow only one giveaway event per server
+      if (giveaways.has(message.guild.id)) {
+        return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'busy'), Bastion.strings.error(message.guild.language, 'isEventInUse', true, 'giveaway'), message.channel);
+      }
+
+      // Giveaway item name
+      args.item = args.item.join(' ');
+
+      // Check if timeout is withing 12 hours
+      if (args.timeout < 1 || args.timeout > 12) {
+        return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'invalidInput'), 'Giveaway can only run for at least an hour and at most 12 hours.', message.channel);
+      }
+
+      // Generate a random reaction for the giveaway message
+      let reaction = [ '🎈', '🎊', '🎉', '🎃', '🎁', '🔮', '🎀', '🎐', '🏮' ];
+      reaction = reaction[Math.floor(Math.random() * reaction.length)];
+
+      // Send the giveaway message and add the reaction to it
+      let giveawayMessage = await message.channel.send({
+        embed: {
+          color: Bastion.colors.BLUE,
+          title: '🎉 GIVEAWAY! 🎉',
+          description: `Giveaway event started. React to this message with ${reaction} to get a chance to win **${args.item}**.`,
+          footer: {
+            text: `Giveaway ID: ${message.guild.id} • Giveaway ends in ${args.timeout} hours from now.`
+          }
+        }
+      });
+      await giveawayMessage.react(reaction);
+
+      // Giveaway message details
+      let giveawayMessageID = giveawayMessage.id;
+
+      // Start giveaway timeout
+      let giveaway = Bastion.setTimeout(async () => {
+        try {
+          // Fetch the giveaway message to get new reactions
+          giveawayMessage = await message.channel.fetchMessage(giveawayMessageID);
+
+          // Get (only) the users who reacted to the giveaway message
+          let participants;
+          if (giveawayMessage.reactions.has(reaction)) {
+            participants = giveawayMessage.reactions.get(reaction).users.filter(user => !user.bot).map(u => u.id);
+          }
+
+          // Get a random user (winner) from the participants
+          let winner;
+          while (!winner && participants.length) {
+            winner = participants[Math.floor(Math.random() * participants.length)];
+            participants.splice(participants.indexOf(winner), 1);
+            winner = await Bastion.fetchUser(winner).catch(() => {});
+          }
+
+          // If there's a winner declare the result
+          if (winner) {
+            // Declare the result in the channel
+            giveawayMessage.edit({
+              embed: {
+                color: Bastion.colors.BLUE,
+                title: 'Giveaway Event Ended',
+                description: `${winner} won **${args.item}**! And will be contacted by ${message.author.tag} with their reward.\nThank you everyone for participating. Better luck next time.`,
+                footer: {
+                  text: `Giveaway ID: ${message.guild.id}`
+                }
+              }
+            }).catch(e => {
+              if (e.code !== 50001) {
+                Bastion.log.error(e);
+              }
+            });
+
+            // Let the winner know via DM
+            winner.send({
+              embed: {
+                color: Bastion.colors.BLUE,
+                title: 'Congratulations',
+                description: `You won the **${args.item}** in a giveaway you participated in **${message.guild.name}** Server!\nYou'll soon be contacted by ${message.author.tag} with your reward.`
+              }
+            }).catch(e => {
+              if (e.code !== 50007) {
+                Bastion.log.error(e);
+              }
+            });
+          }
+          // Otherwise state the unfortunate outcome
+          else {
+            giveawayMessage.edit({
+              embed: {
+                color: Bastion.colors.RED,
+                title: 'Giveaway Event Ended',
+                description: `Unfortunately, no one participated and apparently there's no winner for **${args.item}**. 😕`,
+                footer: {
+                  text: `Giveaway ID: ${message.guild.id}`
+                }
+              }
+            }).catch(e => {
+              if (e.code !== 50001) {
+                Bastion.log.error(e);
+              }
+            });
+          }
+
+          // Remove the giveaway details from cache
+          giveaways.delete(message.guild.id);
+        }
+        catch (e) {
+          Bastion.log.error(e);
+        }
+      }, args.timeout * 60 * 60 * 1000);
+
+      // Store the giveaway information in cache.
+      giveaways.set(message.guild.id, giveaway);
+    }
+    else if (args.end) {
+      if (giveaways.has(message.guild.id)) {
+        // Clear the giveaway timeout
+        Bastion.clearTimeout(giveaways.get(message.guild.id));
+
+        // Remove the giveaway details from cache
+        giveaways.delete(message.guild.id);
+
+        message.channel.send({
+          embed: {
+            color: Bastion.colors.RED,
+            title: 'Giveaway Cancelled',
+            description: `The giveaway event with ID **${message.guild.id}** has been cancelled by ${message.author.tag}`
+          }
+        }).catch(e => {
+          Bastion.log.error(e);
+        });
+      }
+      else {
+        return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'notFound'), 'There\'s no giveaway running in this server right now.', message.channel);
+      }
+    }
+    else {
+      /**
+      * The command was ran with invalid parameters.
+      * @fires commandUsage
+      */
+      return Bastion.emit('commandUsage', message, this.help);
+    }
+  }
+  catch (e) {
+    Bastion.log.error(e);
+  }
+};
+
+exports.config = {
+  aliases: [],
+  enabled: true,
+  argsDefinitions: [
+    { name: 'item', type: String, multiple: true, defaultOption: true },
+    { name: 'timeout', type: Number, alias: 't', defaultValue: 3 },
+    { name: 'end', type: Boolean }
+  ],
+  ownerOnly: false
+};
+
+exports.help = {
+  name: 'giveaway',
+  botPermission: '',
+  userTextPermission: 'ADMINISTRATOR',
+  userVoicePermission: '',
+  usage: 'giveaway < GIVEAWAY ITEM NAME | --end > [-t TIMEOUT_IN_HOURS]',
+  example: [ 'giveaway Awesome Goodies! -t 2', 'giveaway --end' ]
+};
+var user = {};
+var warn = {};
+client.on('message', async function(message) {
+    	 if (!message.channel.guild) return;
+let muteRole1 = message.guild.roles.find("name", "Muted");
+     if (!muteRole1) return;
+  if (message.author.id == client.user.id) return;
+  if(JSON.stringify(user).indexOf(message.author.id) == -1) {
+    user[message.author.id] = message.createdTimestamp;
+    return;
+  } else {
+    if (Date.now() - user[message.author.id] < 695){
+              message.author.delete
+      if (JSON.stringify(warn).indexOf(message.author.id) == -1) {
+        warn[message.author.id] = 1;
+      } else {
+        warn[message.author.id]++;
+        message.author.delete
+      }
+      if (warn[message.author.id] < 6) {
+        message.author.delete
+
+      }
+      delete user[message.author.id];
+              message.author.delete
+
+    } else {
+      delete user[message.author.id];
+              message.author.delete
+
+    }
+  }
+  if (warn[message.author.id] == 6) {
+     if (!message.channel.guild) return;
+             message.author.delete
+
+let muteRole1 = message.guild.roles.find("name", "Muted");
+if(!muteRole1) {
+        muteRole1 = await message.guild.createRole({
+          name: "Muted",
+          color: "#ffffff",
+          permissions:[]
+        })
+        message.guild.channels.forEach(async (channel, id) => {
+          await channel.overwritePermissions(muteRole1, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false,
+			READ_MESSAGES_HISTORY:false
+        });
+		});
+  }
+     if (!muteRole1) return;
+    var guild = message.channel.guild;
+          var currentTime = new Date(),
+            Year = currentTime.getFullYear(),
+            Month = currentTime.getMonth() + 1,
+            Day = currentTime.getDate(),
+            hours = currentTime.getHours() + 3 ,
+            minutes = currentTime.getMinutes()+1,
+            seconds = currentTime.getSeconds();
+
+           if (!message.channel.guild) return;
+     if (!muteRole1) return;
+    var guild = message.channel.guild;
+    message.guild.members.get(message.author.id).addRole(muteRole1);
+	setTimeout(function(){
+		    message.guild.members.get(message.author.id).removeRole(muteRole1);
+	},7200000);
+     var msg;
+        msg = parseInt();
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+delete warn[message.author.id];
+    delete user[message.author.id];
+	const embed500 = new Discord.RichEmbed()
+     .setTitle(`mark:  | هناك احد يحاول `)
+      .setDescription(":white_check_mark:  | `There is someone trying to do spam`\n\nName:\n"+`${message.author.username}#${message.author.discriminator}`+"\nThe required procedures have been taken")      .setColor("ff0000")
+    message.channel.send(embed500)
+    	const embed20 = new Discord.RichEmbed()
+      .setTitle(":scales: | تم معاقبتك")
+      .setDescription(`**You have been Muted **\n\nBy:\n${client.user.tag}\n\nThe reason:\nSpam Chat\n\nMuted Date:\n`+ Year + "/" + Month + "/" + Day +', '+hours +'-' +minutes+'-'+seconds+"\n \n \n`If the punishment by mistake continues with the administration \n\nTime of unmute : Two hours after the date of the death`")
+          .setFooter(message.guild.iconURL)
+      .setColor("ff0000")
+
+     message.author.send(embed20)
+
+  }
+});
+
 client.on('message', message => {
 var prefix = "#";
 
