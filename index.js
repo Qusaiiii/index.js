698a71f444dfcf69e4b9ecc8b28a5f5f65ceaa83
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-07-31 23:27:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,6 @@ gg.send({embed : new Discord.RichEmbed()//By Codes , - ST I EdiTeD , .#4968//By 
 });
 }        
 })
-})//By Codes , - ST I EdiTeD , .#4968
 })
  }//By Codes ,
  client.on('message', message => {
