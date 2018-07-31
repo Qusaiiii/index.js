a2683b08392e72f4f599e50bc4fcae58934be8af
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-07-31 22:21:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,36 @@ client.on('message', message => {
       message.channel.sendEmbed(embed);
     }
 });
+ client.on('message', message => {
+    if (message.content.startsWith(prefix + "id")) {
+var args = message.content.split(" ").slice(1);
+let user = message.mentions.users.first();
+var men = message.mentions.users.first();
+ var heg;
+ if(men) {
+     heg = men
+ } else {
+     heg = message.author
+ }
+var mentionned = message.mentions.members.first();
+  var h;
+ if(mentionned) {
+     h = mentionned
+ } else {
+     h = message.member
+ }
+        moment.locale('ar-TN');
+var id = new  Discord.RichEmbed()
+.setColor("RANDOM")
+.addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)
+.addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true)
+.addField(": النك نيم",`${h.nickname}`, true) .addField(": #",heg.discriminator, true)
+.addField(`: البلينق`,`${h.presence.game && h.presence.game.name || '-'}`,true) .addField(': الحالة',`${h.presence.status}`,true)
+.addField(`: الرتب`, `${message.guild.members.get(h.id).roles.map(r => `\`${r.name}\``).slice(1).join('\n') || 'لايوجد رتب'}`,true)                                                    
+.setThumbnail(heg.avatarURL);
+message.channel.send(id)
+}       });
+  
 let giveaways = new Map();
 
 exports.exec = async (Bastion, message, args) => {
@@ -210,12 +240,12 @@ exports.config = {
 };
 
 exports.help = {
-  name: 'giveaway',
+  name: '#giveaway',
   botPermission: '',
   userTextPermission: 'ADMINISTRATOR',
   userVoicePermission: '',
-  usage: 'giveaway < GIVEAWAY ITEM NAME | --end > [-t TIMEOUT_IN_HOURS]',
-  example: [ 'giveaway Awesome Goodies! -t 2', 'giveaway --end' ]
+  usage: '#giveaway < GIVEAWAY ITEM NAME | --end > [-t TIMEOUT_IN_HOURS]',
+  example: [ '#giveaway Awesome Goodies! -t 2', 'giveaway --end' ]
 };
 var user = {};
 var warn = {};
@@ -540,7 +570,8 @@ if (message.content.startsWith(prefix + 'Help')) { /// This is The DMS Code Send
 9༺༻  #level | Level༺༻
 10༺༻  #كت تويت | Cut-Tweet༺༻
 10༺༻ #Discrim | Discrim༺༻
-11༺༻  مميزات البوت༺༻
+11༺༻ #id | ID༺༻
+12༺༻  مميزات البوت༺༻
 - Tickets
 - AutoRole
 - Welcome
@@ -548,6 +579,7 @@ if (message.content.startsWith(prefix + 'Help')) { /// This is The DMS Code Send
 - Colors
 - Music
 - Anti-Links
+- Anti-Spam
 ༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻
 Click On ▶ To Go Administor Side
    `
